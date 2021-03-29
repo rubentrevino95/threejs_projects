b2cf8e375036bdfce32f042e3be6bf27f9bd4f1c
Ruben <rubentrevino95@gmail.com>
2021-03-29 18:03:56 -0500

    stack game init

--- shapes/src/App.js
+++ shapes/src/App.js
@@ -4,30 +4,67 @@ import * as THREE from 'three';
 
 function App() {
 
-  const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      const scene = new THREE.Scene();
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
+			const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 45, 30000 );
+      camera.position.set(-900,-200,-900);
+			const renderer = new THREE.WebGLRenderer({antialias:true});
+			renderer.setSize( window.innerWidth, window.innerHeight );  
 			document.body.appendChild( renderer.domElement );
 
-			const geometry = new THREE.BoxGeometry();
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+      let controls = new THREE.OrbitControls(camera);
+      controls.addEventListener('change', renderer);
+      controls.minDistance = 500;
+      controls.maxDistance = 1500;
 
-			camera.position.z = 5;
+      let materialArray = [];
+      let texture_ft = new THREE.TextureLoader().load('envmap_interstellar\interstellar_ft.tga');
+      let texture_bk = new THREE.TextureLoader().load('envmap_interstellar\interstellar_bk.tga');
+      let texture_dn = new THREE.TextureLoader().load('envmap_interstellar\interstellar_dn.tga');
+      let texture_lf = new THREE.TextureLoader().load('envmap_interstellar\interstellar_lf.tga');
+      let texture_rt = new THREE.TextureLoader().load('envmap_interstellar\interstellar_rt.tga');
+      let texture_up = new THREE.TextureLoader().load('envmap_interstellar\interstellar_up.tga');
 
-			const animate = function () {
-				requestAnimationFrame( animate );
+      materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+      materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+      materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}));
+      materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}));
+      materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+      materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
+      for(let i = 0; i<6; i++) {
+        materialArray[i].side = THREE.BackSide;
+      }
 
-				renderer.render( scene, camera );
-			};
+      let skyboxGeo = new THREE.BoxGeometry(10000,10000,10000);
+      let skybox = new THREE.Mesh(skyboxGeo, materialArray);
+      scene.add(skybox);
 
-			animate();
+      function animate() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+      }
+      init();
+
+
+
+			// const geometry = new THREE.BoxGeometry();
+			// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			// const cube = new THREE.Mesh( geometry, material );
+			// scene.add( cube );
+
+			// camera.position.z = 5;
+
+			// const animate = function () {
+			// 	requestAnimationFrame( animate );
+
+			// 	cube.rotation.x += 0.01;
+			// 	cube.rotation.y += 0.01;
+
+			// 	renderer.render( scene, camera );
+			// };
+
+			// animate();
 
   return (
     <div className="App">
